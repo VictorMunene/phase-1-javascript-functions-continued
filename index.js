db89fcc79813a7639baf74e269b7f1d7a36c07d0
VictorMunene <victor.munene@student.moringaschool.com>
2022-08-20 00:01:14 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (grind = 'go to the office') {
+    return `This Monday, I will ${grind}.`
+}
+
+function wrapAdjective (wrap = '*') {
+    return function (result = 'special') {
+        return `You are ${wrap}${result}${wrap}!`
+    }
+}
